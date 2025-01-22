eac0dd9557c838bcc99cfefef3b67aa9f9d6a77d
Perttu Haliseva <perttu@gmail.com>
2025-01-22 14:45:41 +0200

    Update agenda, add small versions of city photos

--- src/Cities.ts
+++ src/Cities.ts
@@ -2,35 +2,45 @@ export const cities = [
   {
     name: 'Stockholm',
     date: '18.4.2025',
-    imageUrl: '/assets/stockholm.webp',
+    imageUrl: '/assets/stockholm_small.webp',
     agenda: `Friday, April 18th, 2025
 
-    Running time to be confirmed.
+    Run in Stockholm hosted by MRC Stockholm. Time to be confirmed.
 
-    Boat 17:30 Tallink/Silja Baltic Queen to Tallinn.
-    
-    Arrival Tallinn Saturday 10:45.`,
+    Post-run beers and party.
+
+    Boat 17:30 Tallink Silja Baltic Queen to Tallinn; overnight trip, arrival to Tallinn Saturday 10:45.`,
   },
   {
     name: 'Tallinn',
     date: '19.4.2025',
-    imageUrl: '/assets/tallinn.webp',
+    imageUrl: '/assets/tallinn_small.webp',
     agenda: `Saturday, April 19th, 2025
 
-    Running time to be confirmed.
+    Run in Tallinn hosted by Pühaste jooksuklubi.
 
-    Boat 22:30 Tallink/Silja Megastar to Helsinki.`,
+    Post-run beers and party.
+
+    Boat 22:30 Tallink Silja Megastar to Helsinki (2 hour trip, arrives 00:30)
+
+   Note! Feel free to book earlier or later boat to suit your needs eg. depending on choice of hotel and if you want to overnight in Helsinki (recommended for those who want to sleep late) or Tallinn (if you prefer to catch an early boat at around 6 or 7).`,
   },
   {
     name: 'Helsinki',
     date: '20.4.2025',
-    imageUrl: '/assets/helsinki.webp',
+    imageUrl: '/assets/helsinki_small.webp',
     agenda: `Sunday, April 20th, 2025
 
-    Run 12:30 starting from SalamaNation.
+    Run in Helsinki hosted by MRC Helsinki. Starting at 12:30 from SalamaNation.
 
-    Boat 17:00 Silja Serenade to Stockholm.
+    Post-run beers and party at SalamaNation.
+
+    Optional beer mile (to be confirmed).
+
+    Awards and farewells until we meet again.
+
+    Suggested boat for Stockholm-bound runners: 17:00 Silja Serenade to Stockholm. Arrives to STO on Monday 10:00.
     
-    Arrival Monday 10:00.`,
+    Note! Feel free to book another boat or flight, or stay in town until Monday.`,
   },
 ];
--- src/components/CityCard.tsx
+++ src/components/CityCard.tsx
@@ -1,29 +1,29 @@
 interface CityCardProps {
-  city: string;
-  date: string;
-  imageUrl: string;
-  onClick: () => void;
+	city: string;
+	date: string;
+	imageUrl: string;
+	onClick: () => void;
 }
 
 const CityCard = ({ city, date, imageUrl, onClick }: CityCardProps) => {
-  return (
-    <div
-      style={{ backgroundImage: `url("${imageUrl}")` }}
-      className='bg-center bg-cover shadow-lg rounded-md hover:shadow-xl overflow-hidden flex flex-col items-center justify-between w-full h-80 p-4'
-    >
-      <h3 className='bg-brand-paper px-4'>{city}</h3>
-      <div className='p-4 text-center'>
-        <p className='text-gray-500 bg-brand-paper'>{date}</p>
-        <button
-          type='button'
-          onClick={onClick}
-          className='mt-8 bg-brand-paper text-white px-4 py-2 rounded hover:bg-blue-600 cursor-pointer'
-        >
-          View Agenda
-        </button>
-      </div>
-    </div>
-  );
+	return (
+		<div
+			style={{ backgroundImage: `url("${imageUrl}")` }}
+			className="bg-center bg-cover shadow-lg rounded-md hover:shadow-xl overflow-hidden flex flex-col items-center justify-between w-full h-80 p-4"
+		>
+			<h3 className="bg-brand-paper px-4">{city}</h3>
+			<div className="p-4 text-center">
+				<p className="text-gray-500 bg-brand-paper">{date}</p>
+				<button
+					type="button"
+					onClick={onClick}
+					className="mt-8 bg-brand-paper text-white px-4 py-2 rounded hover:bg-blue-600 cursor-pointer"
+				>
+					View Agenda
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default CityCard;
