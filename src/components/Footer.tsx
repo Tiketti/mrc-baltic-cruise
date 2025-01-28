45f5e89ae011bd8a14f7711e450218b3841934ef
Perttu Haliseva <perttu@gmail.com>
2025-01-28 19:20:10 +0200

    Make footer image full width

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,7 @@
 export const Footer = () => {
   return (
-    <div
-      style={{ backgroundImage: `url("assets/mrc_cruise_map.svg")` }}
-      className="flex text-center justify-center p-8 bg-center bg-cover h-72 md:h-96 lg:h-[30rem] lg:bg-center"
-    />
+    <div className="flex text-center justify-center w-full p-0">
+      <img src="assets/mrc_cruise_map.svg" alt="Cruise map" />
+    </div>
   );
 };
