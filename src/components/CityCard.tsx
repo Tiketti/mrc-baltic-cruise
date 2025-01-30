6335615038c34b00c9438418003e906b5393d6dc
Perttu Haliseva <perttu@gmail.com>
2025-01-30 10:29:13 +0200

    Adjust city card title width

--- src/components/CityCard.tsx
+++ src/components/CityCard.tsx
@@ -20,7 +20,7 @@ const CityCard = ({
       }}
       className="bg-center bg-cover shadow-lg rounded-md hover:shadow-xl overflow-hidden flex flex-col items-center justify-between w-full h-80 p-4"
     >
-      <h3 className="bg-brand-paper px-4">{city}</h3>
+      <h3 className="bg-brand-paper px-16 max-w-fit">{city}</h3>
       <div className="p-4 text-center">
         <p className="text-gray-500 bg-brand-paper">{date}</p>
         <button
