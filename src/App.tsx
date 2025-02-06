63fe21dd9189f3bdf2c7fc8739e55ead10ca66cd
Perttu Haliseva <perttu@gmail.com>
2025-02-06 13:25:53 +0200

    Extract Strava event to a separate component
    Couldn't think of a better name than Midfooter - oh well.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Link } from "lucide-react";
 import { useState } from "react";
+import { MidFooter } from "./Midfooter";
 import CityCard from "./components/CityCard";
 import { Dialog } from "./components/Dialog";
 import { Faq } from "./components/Faq";
@@ -51,19 +52,7 @@ function App() {
         <Faq items={faqItems} />
         <HostsSection />
       </div>
-      <div id="strava" className="flex justify-center">
-        <a
-          href="https://www.strava.com/clubs/315734/group_events/1892944"
-          className="inline-flex items-center gap-1 bg-brand-paper p-4 h-min"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src="assets/strava-seeklogo.svg" alt="Logo" className="h-6" />
-          <span className="text-brand-burgundy border-b border-brand-paper hover:border-brand-burgundy font-semibold text-xl">
-            Strava event
-          </span>
-        </a>
-      </div>
+      <MidFooter />
       <Footer />
     </div>
   );
---
+++ src/Midfooter.tsx
@@ -0,0 +1,15 @@
+export const MidFooter = () => (
+  <div id="strava" className="flex justify-center">
+    <a
+      href="https://www.strava.com/clubs/315734/group_events/1892944"
+      className="inline-flex items-center gap-1 bg-brand-paper p-4 h-min"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <img src="assets/strava-seeklogo.svg" alt="Logo" className="h-6" />
+      <span className="text-brand-burgundy border-b border-brand-paper hover:border-brand-burgundy font-semibold text-xl">
+        Strava event
+      </span>
+    </a>
+  </div>
+);
