5757e81af8fdd9e221722c7582a9d52c85ce5118
Perttu Haliseva <perttu@gmail.com>
2025-03-12 11:17:33 +0200

    Add STO Strava event

--- src/Midfooter.tsx
+++ src/Midfooter.tsx
@@ -1,14 +1,30 @@
 export const MidFooter = () => (
-  <div id="strava" className="flex justify-center">
+  <div
+    id="strava"
+    className="flex flex-col min-[500px]:flex-row justify-center"
+  >
     <a
       href="https://www.strava.com/clubs/315734/group_events/1892944"
-      className="inline-flex items-center gap-1 bg-brand-paper p-4 h-min"
+      className="inline-flex items-center justify-center gap-1 bg-brand-paper p-4 h-min"
       target="_blank"
       rel="noreferrer"
+      title="Main Strava event"
     >
       <img src="assets/strava-seeklogo.svg" alt="Logo" className="h-6" />
       <span className="text-brand-burgundy border-b border-brand-paper hover:border-brand-burgundy font-semibold text-xl">
-        Strava event
+        Main Strava event
+      </span>
+    </a>
+    <a
+      href="https://www.strava.com/clubs/302996/group_events/1948062"
+      className="inline-flex items-center justify-center gap-1 bg-brand-paper p-4 h-min"
+      target="_blank"
+      rel="noreferrer"
+      title="Stockholm event"
+    >
+      <img src="assets/strava-seeklogo.svg" alt="Logo" className="h-6" />
+      <span className="text-brand-burgundy border-b border-brand-paper hover:border-brand-burgundy font-semibold text-xl">
+        Stockholm event
       </span>
     </a>
   </div>
