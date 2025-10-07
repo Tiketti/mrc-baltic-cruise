45916c6f03257e2799d89660bddf612a2f9e2441
Perttu Haliseva <perttu@gmail.com>
2025-10-07 11:34:59 +0300

    Use Garmin instead of Komoot for map embed

--- src/components/MapTab.tsx
+++ src/components/MapTab.tsx
@@ -1,19 +1,17 @@
 export const MapTab = () => {
   return (
-    <div className="space-y-4">
+    <div className="flex h-[calc(100vh-12rem)] flex-col pb-4">
       {/* Map Section */}
-      <div className="rounded-lg bg-gray-800 px-4">
-        <h2 className="mb-4 font-semibold text-lg">Route Map</h2>
+      <div className="flex flex-1 flex-col overflow-hidden rounded-lg bg-gray-800">
+        <h2 className="px-4 pt-4 pb-2 font-semibold text-lg">Route Map</h2>
 
-        <div className="overflow-hidden rounded bg-gray-700">
+        <div className="mx-4 mb-4 flex-1 overflow-hidden rounded bg-gray-700">
           <iframe
-            src="https://www.komoot.com/tour/2505302520/embed?share_token=aMsEPjpxi5BneySQiuFEE14Iw3TOcdAvHAwJjgnBRSuNNZo1s2"
+            src="https://connect.garmin.com/modern/course/embed/409837108"
             width="100%"
-            height="600"
-            frameBorder="0"
-            scrolling="no"
-            title="Brewery Run Route"
-            className="w-full"
+            height="100%"
+            title="Course Embed"
+            className="h-full w-full"
           />
         </div>
       </div>
